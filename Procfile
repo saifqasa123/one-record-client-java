4c1985940eaf1cbb5d3cf15b773fc6d91c3b8fee
Andra Blaj <blaja@iata.org>
2020-04-30 13:53:22 +0200

    Try stuff

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/onerecord-client-0.0.1-SNAPSHOT.jar 
--- src/main/java/org/iata/config/WebSecurityConfig.java
+++ src/main/java/org/iata/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.requiresChannel()
-        .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+        .anyRequest()
         .requiresSecure();
   }
 }
