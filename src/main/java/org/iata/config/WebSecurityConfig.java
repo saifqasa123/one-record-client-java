cdcab0664b5d6fcd52915981260bb50c030693d1
Andra Blaj <blaja@iata.org>
2020-04-30 16:46:11 +0200

    Try more stuff

--- src/main/java/org/iata/config/WebSecurityConfig.java
+++ src/main/java/org/iata/config/WebSecurityConfig.java
@@ -1,19 +1,20 @@
 package org.iata.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Configure Spring Security to require a secure channel for all requests
  */
-@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.requiresChannel()
-        .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+        .anyRequest()
+        //.requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
         .requiresSecure();
   }
 }
