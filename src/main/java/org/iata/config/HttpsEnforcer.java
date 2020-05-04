3e1d66cbc508c4951a488fd6255ff6353832a894
Andra Blaj <blaja@iata.org>
2020-05-04 14:26:32 +0200

    Try stuff

--- src/main/java/org/iata/config/HttpsEnforcer.java
+++ src/main/java/org/iata/config/HttpsEnforcer.java
@@ -21,7 +21,6 @@ public class HttpsEnforcer implements Filter {
 
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException, IOException {
-    System.out.println("HERE");
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     HttpServletResponse response = (HttpServletResponse) servletResponse;
 
